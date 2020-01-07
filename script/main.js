96bc11fedfb2006180a8719843494b15695fba9a
ayham <ayham.alaaeddin@gmail.com>
2020-01-07 16:22:46 +0100

    read only

--- script/main.js
+++ script/main.js
@@ -1,12 +1,12 @@
 var cryptoEx;
 let firstInput = document.querySelector("#myInput1")
 let secondInput = document.querySelector("#myInput2")
-let option1 = document.querySelector("#inputGroupSelect01").value
-let option2 = document.querySelector("#inputGroupSelect02").value
-
+let option1 = document.querySelector("#inputGroupSelect01")
+let option2 = document.querySelector("#inputGroupSelect02")
+let button = document.querySelector(".submit")
 
 function getData(){
-    let url = "https://api.cryptonator.com/api/ticker/btc-usd"
+    let url = `https://api.cryptonator.com/api/ticker/${option1.value}-${option2.value}`
 
     fetch(url)
         .then(data => data.json())
@@ -15,8 +15,22 @@ function getData(){
             
             console.log(cryptoEx)
         })
+        .then(convert)
 
 }
 
-console.log(option1)
+console.log(option1.value)
 getData()
+
+function convert() {
+    var priceNum = cryptoEx.ticker.price
+    priceNum = parseFloat(priceNum)
+    var finalPrice = firstInput.value * priceNum
+
+
+   secondInput.value = finalPrice
+}
+
+ 
+
+button.addEventListener("click", getData)
